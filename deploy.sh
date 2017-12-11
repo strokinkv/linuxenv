747c022a5bf5c3f1d33bf30e12dbfd7f727dff7e
Строкин Кирилл Владимирович <strokinkv@gmail.com>
2017-12-11 10:23:35 +0000

    deploy script with ansible

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-if [ $(basename $PWD) != "linuxenv" ]
-then
-  cd $(locate --basename linuxenv)
-fi
-
-# bash
-sed -i -e '/alias \(cp\|rm\|mv\)=.*\(cp\|rm\|mv\) -i/d' ~/.bashrc
-cp -f ./bash/.bash_profile ~/.bash_profile
-
-# vim
-cp -f ./vim/.vimrc ~/.vimrc
-
-# git
-cp -f ./git/.gitconfig ~/.gitconfig
-
-# tmux
-cp -f ./tmux/.tmux.conf ~/.tmux.conf
-
-# yum
-yum -y install $(cat yum/rpmlist)
+yum install -y git ansible
+mkdir ~/git
+cd ~/git
+git clone https://github.com/strokinkv/linuxenv.git
+ansible-playbook ~/git/linuxenv/ansible/localhost.yml
